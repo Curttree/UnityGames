d39b0941cf64c90b421cb94e0654ab8a1fda8426
curttree <curttremblay@gmail.com>
2020-05-07 00:42:19 -0400

    Start of Controllers for Flappy Bird Clone
    Starting to build shared assets based on tutorials. Menus.

---
+++ Bounce/Assets/Flappy Scripts/GameController.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public static GameController instance;
+
+    private const string HIGH_SCORE = "High Score";
+
+    private const string SELECTED_BIRD = "Selected Bird";
+
+    private const string GREEN_BIRD = "Green Bird";
+
+    private const string RED_BIRD = "Red Bird";
+
+
+    private void Awake()
+    {
+        PlayerPrefs.DeleteAll();
+        MakeSingleton();
+        IsTheGameStartedForTheFirstTime();
+    }
+    // Start is called before the first frame update
+    void IsTheGameStartedForTheFirstTime()
+    {
+        if (!PlayerPrefs.HasKey("FirstTime"))
+        {
+            PlayerPrefs.SetInt(HIGH_SCORE, 0);
+            PlayerPrefs.SetInt(SELECTED_BIRD, 0);
+            PlayerPrefs.SetInt(GREEN_BIRD, 0);
+            PlayerPrefs.SetInt(RED_BIRD, 0);
+            PlayerPrefs.SetInt("FirstTime", 0);
+        }
+    }
+
+    public void SetHighScore(int score)
+    {
+        PlayerPrefs.SetInt(HIGH_SCORE, score);
+    }
+
+    public int GetHighScore(int score)
+    {
+        return PlayerPrefs.GetInt(HIGH_SCORE);
+    }
+
+    public void SetSelectedBird(int selectedBird)
+    {
+        PlayerPrefs.SetInt(SELECTED_BIRD, selectedBird);
+    }
+
+    public int GetSelectedBird()
+    {
+        return PlayerPrefs.GetInt(SELECTED_BIRD);
+    }
+
+    public void UnlockGreenBird()
+    {
+        PlayerPrefs.SetInt(GREEN_BIRD, 1);
+    }
+
+    public bool IsGreenBirdUnlocked()
+    {
+        return PlayerPrefs.GetInt(GREEN_BIRD) == 1;
+    }
+
+    public void UnlockRedBird()
+    {
+        PlayerPrefs.SetInt(RED_BIRD, 1);
+    }
+
+    public bool IsRedBirdUnlocked()
+    {
+        return PlayerPrefs.GetInt(RED_BIRD) == 1;
+    }
+
+    void MakeSingleton()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ Bounce/Assets/Flappy Scripts/MenuController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public static MenuController instance;
+
+    [SerializeField]
+    private GameObject[] birds;
+
+    private bool isGreenBirdUnlocked, isRedBirdUnlocked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        birds[GameController.instance.GetSelectedBird()].SetActive(true);
+        CheckIfUnlocked();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneFader.instance.FadeIn("gameplay");
+    }
+
+    void CheckIfUnlocked()
+    {
+        if (GameController.instance.IsGreenBirdUnlocked())
+        {
+            isGreenBirdUnlocked = true;
+        }
+        if (GameController.instance.IsRedBirdUnlocked())
+        {
+            isRedBirdUnlocked = true;
+        }
+    }
+
+    public void ChangeBird()
+    {
+        if (GameController.instance.GetSelectedBird() == 0)
+        {
+            Debug.Log(isGreenBirdUnlocked.ToString());
+            if (isGreenBirdUnlocked)
+            {
+                birds[0].SetActive(false);
+                GameController.instance.SetSelectedBird(1);
+                birds[GameController.instance.GetSelectedBird()].SetActive(true);
+            }
+        }
+        else if (GameController.instance.GetSelectedBird() == 1)
+        {
+            if (isRedBirdUnlocked)
+            {
+                GameController.instance.SetSelectedBird(2);
+            }
+            else
+            {
+                GameController.instance.SetSelectedBird(0);
+            }
+
+            birds[1].SetActive(false);
+            birds[GameController.instance.GetSelectedBird()].SetActive(true);
+        }
+        else if (GameController.instance.GetSelectedBird() == 2)
+        {
+            birds[2].SetActive(false);
+            GameController.instance.SetSelectedBird(0);
+            birds[GameController.instance.GetSelectedBird()].SetActive(true);
+
+        }
+    }
+}
